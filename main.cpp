45b53c34dbb456e4f8ae338cd95a2e282c6d37b3
Samuele Mazzei <59342479+Samu01Tech@users.noreply.github.com>
2021-09-22 15:22:08 +0000

    static, const e robe che non funzionano

--- main.cpp
+++ main.cpp
@@ -4,18 +4,36 @@ using namespace std;
 class A{
     private:
         int k;
-        const float mc = 3.14;
+        const float mc;
+        static int numistanze;
+        static const int a = 4;
+        static const float b;
     public:
-        A(){
+        A():mc(3.14){
             k = 0;
+            numistanze++;
+            cout << "Istanze: " << numistanze << endl;
         };
-        A(int _k){
+        A(int _k, float _mc):mc(_mc){
             k = _k;
+            numistanze++;
+            cout << "Istanze: " << numistanze << endl;
         };
         ~A(){
+            numistanze--;
+            cout << "Istanze: " << numistanze << endl;
         };
+        int getNI(){
+            return numistanze;
+        }
 };
 
+int A::numistanze = 0;
+const float A::b = 0.05;
+
 int main() {
-  cout << "Hello Michael!\n";
+    A a1;
+    A a2(3, 6.28);
+    A a3;
+    return 0;
 }
